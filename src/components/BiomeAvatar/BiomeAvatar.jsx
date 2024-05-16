9f9138a0b66fed13b1e108dfd523b5398313646e
iSayZ <estrine.alexis@gmail.com>
2024-05-16 22:37:44 +0200

    fix

--- src/components/BiomeAvatar/BiomeAvatar.jsx
+++ src/components/BiomeAvatar/BiomeAvatar.jsx
@@ -7,7 +7,7 @@ function BiomeAvatar({ data }) {
   return (
     <div className="avatar_container">
       <div className="avatar_description">
-        <img src={data.title} className="avatar_title" alt="Titre de l'image" />
+        <img src={data.titleSrc} className="avatar_title" alt="Titre de l'image" />
         <div className="avatar_card">
           <div className="avatar_card_skin">
             <p className="avatar_skin_title">{data.avatarName}</p>
