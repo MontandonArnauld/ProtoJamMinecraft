ed1c169d8b97b2194ae0e1b0605ba9bde5ca011d
Your Name <you@example.com>
2024-05-16 20:17:05 +0200

    maj biome

--- src/components/BiomeAvatar/BiomeAvatar.jsx
+++ src/components/BiomeAvatar/BiomeAvatar.jsx
@@ -6,16 +6,20 @@ function BiomeAvatar({ data }) {
 
   return (
     <div className="avatar_container">
-      <div className="avatar_card">
+      <div className="avatar_description">
         <h2 className="avatar_title">{data.title}</h2>
-        <p>{data.avatarName}</p>
-        <ReactSkinview3d
-          skinUrl="textures/skin1.png"
-          capeUrl="textures/mojang-classic-cape.png"
-          height="200"
-          width="200"
-        />
-        <p>{data.avatarText}</p>
+        <div className="avatar_card">
+          <div className="avatar_card_skin">
+            <p className="avatar_skin_title">{data.avatarName}</p>
+            <ReactSkinview3d
+              skinUrl="textures/skin1.png"
+              capeUrl="textures/mojang-classic-cape.png"
+              height="200"
+              width="200"
+            />
+          </div>
+          <p>{data.avatarText}</p>
+        </div>
       </div>
     </div>
   );
