c527054b9f1e087604394f17ee522b19bbc60242
iSayZ <estrine.alexis@gmail.com>
2024-05-17 14:22:38 +0200

    final commit

--- src/components/HouseDescription/HouseDescription.jsx
+++ src/components/HouseDescription/HouseDescription.jsx
@@ -5,7 +5,7 @@ function HouseDescription({ data }) {
   return (
     <div className={`house_container ${data.title}`}>
       <div className="house_main">
-        <img className="house_img" src="../assets/images/chalet-projet.png" alt="" />
+        <img className="house_img" src={data.houseImg} alt="" />
         <div className="house_desc">
           <p>{data.houseDescription}</p>
         </div>
